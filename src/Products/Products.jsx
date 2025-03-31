61db00770b13429297a6bd499354779f1ab955e3
Psammy <ampadupsamuel1@gmail.com>
2025-03-31 00:08:43 +0000

    products

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,6 +1,36 @@
 import React from 'react';
-
+import './products.css';
+import { AiFillStar } from 'react-icons/ai';
+import { BsFillBagHeartFill } from 'react-icons/bs';
 function Products() {
-  return <div>Products</div>;
+  return (
+    <section className="card-container">
+      <section className="card">
+        <img
+          src="https://m.media-amazon.com/images/I/61N4GyWcHPL._AC_UY575_.jpg"
+          alt="Image of shoe"
+          className="card-img"
+        />
+        <div className="card-details">
+          <h3 className="card-title">Shoe</h3>
+          <section className="card-reviews">
+            <AiFillStar className="rating-star" />
+            <AiFillStar className="rating-star" />
+            <AiFillStar className="rating-star" />
+            <AiFillStar className="rating-star" />
+            <span className="total-reviews">4</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>$450</del>220
+            </div>
+            <div className="bag">
+              <BsFillBagHeartFill className="bag-icon" />
+            </div>
+          </section>
+        </div>
+      </section>
+    </section>
+  );
 }
 export default Products;
--- src/Recommended/Recommended.jsx
+++ src/Recommended/Recommended.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
-
+import './recommended.css';
 function Recommended() {
-  return <div>Recommended</div>;
+  return (
+    <div>
+      <h2>Recommended </h2>
+      <div className="recommended-btns">
+        <button>All Products</button>
+        <button>Nike</button>
+        <button>Adidas</button>
+        <button>Puma</button>
+        <button>Vans</button>
+      </div>
+    </div>
+  );
 }
 export default Recommended;

66497f95be626c00fd122fac10c5d45409b8f14a
Psammy <ampadupsamuel1@gmail.com>
2025-03-31 15:51:47 +0000

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Nav from './Navigation/Nav';
 import Products from './Products/Products';
 import Recommended from './Recommended/Recommended';
 import Sidebar from './Sidebar/Sidebar';
-
+import products from './db/data';
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
 
-  //Input filter
+  //----------Input filter-----------
   const [query, setQuery] = useState('');
 
-  const handleInputChange = e = {
-    setQuery(e.target.value);
-  }
+  const filteredItems = products.filter(
+    (product) =>
+      product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase) !== -1
+  );
+
+  //----------Radio filter-----------
+  const handleInputChange = (e) => setQuery(e.target.value);
   return (
     <>
       <Sidebar />
